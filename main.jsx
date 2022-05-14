43ad5a9143a2084e5f793ad7d2af72123bca44b2
Michael[tm] Smith <mike@w3.org>
2022-05-15 07:22:07 +0900

    Lowercase “browser” in “non-Browser”
    …there’s no reason for “browser” to be uppercase here

--- main.jsx
+++ main.jsx
@@ -17,7 +17,7 @@ function Home() {
         This community group aims to provide a space for JS runtimes to
         collaborate on API interoperability. We focus on documenting and
         improving interoperability of web platform APIs across runtimes
-        (especially non-Browser ones). This is done through discussions among
+        (especially non-browser ones). This is done through discussions among
         runtimes, proposals for new and existing web APIs in existing
         specification venues (WHATWG, W3C), and documentation of existing
         runtime behaviours.{" "}
